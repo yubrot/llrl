a462362dd3cad28983491f1f1cea4100144112f0
yubrot <yubrot@gmail.com>
2021-06-02 01:02:19 +0900

    Simplify and optimize module::scope implementation

--- llrl0/src/module/scope.rs
+++ llrl0/src/module/scope.rs
@@ -1,6 +1,5 @@
 use super::{pass, Error, LocatedConstruct};
-use smallvec::{smallvec, SmallVec};
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 /// A key-value store for language constructs.
 pub trait Scope: Sized {
@@ -25,7 +24,7 @@ impl<'a, T: Scope> Scope for &'a mut T {
 
 #[derive(Debug, Clone)]
 pub struct TopLevel {
-    map: HashMap<String, SmallVec<[LocatedConstruct; 1]>>,
+    map: HashMap<String, LocatedConstruct>,
 }
 
 impl TopLevel {
@@ -36,15 +35,13 @@ impl TopLevel {
     }
 
     pub fn iter<'a>(&'a self) -> impl Iterator<Item = (&'a str, LocatedConstruct)> + 'a {
-        self.map
-            .iter()
-            .map(|(name, cs)| (name.as_str(), *cs.first().unwrap()))
+        self.map.iter().map(|(name, c)| (name.as_str(), *c))
     }
 }
 
 impl Scope for TopLevel {
     fn get(&self, name: &str) -> Option<LocatedConstruct> {
-        self.map.get(name).and_then(|vec| vec.last().copied())
+        self.map.get(name).copied()
     }
 
     fn enter_scope(&mut self) -> LocalScope {
@@ -52,48 +49,43 @@ impl Scope for TopLevel {
     }
 
     fn define(&mut self, name: &str, c: LocatedConstruct) -> pass::Result<()> {
-        match self.map.insert(name.to_string(), smallvec![c]) {
-            None => Ok(()),
-            Some(vec) => match vec.as_slice() {
-                [old_c, ..] if old_c.construct != c.construct => {
-                    Err(Error::multiple_declarations(name, *old_c, c))
-                }
-                _ => Ok(()),
-            },
+        match self.map.insert(name.to_string(), c) {
+            Some(old_c) if old_c.construct != c.construct => {
+                Err(Error::multiple_declarations(name, old_c, c))
+            }
+            _ => Ok(()),
         }
     }
 }
 
 pub struct LocalScope<'a> {
-    map: &'a mut HashMap<String, SmallVec<[LocatedConstruct; 1]>>,
-    scope_binds: HashSet<String>,
+    map: &'a mut HashMap<String, LocatedConstruct>,
+    stash: HashMap<String, Option<LocatedConstruct>>,
 }
 
 impl<'a> LocalScope<'a> {
-    pub fn new(map: &'a mut HashMap<String, SmallVec<[LocatedConstruct; 1]>>) -> Self {
+    pub fn new(map: &'a mut HashMap<String, LocatedConstruct>) -> Self {
         Self {
             map,
-            scope_binds: HashSet::new(),
+            stash: HashMap::new(),
         }
     }
 }
 
 impl<'a> Drop for LocalScope<'a> {
     fn drop(&mut self) {
-        for name in self.scope_binds.iter() {
-            if let Some(v) = self.map.get_mut(name) {
-                v.pop();
-                if v.is_empty() {
-                    self.map.remove(name);
-                }
-            }
+        for (name, c) in self.stash.drain() {
+            match c {
+                Some(c) => self.map.insert(name, c),
+                None => self.map.remove(&name),
+            };
         }
     }
 }
 
 impl<'a> Scope for LocalScope<'a> {
     fn get(&self, name: &str) -> Option<LocatedConstruct> {
-        self.map.get(name).and_then(|vec| vec.last().copied())
+        self.map.get(name).copied()
     }
 
     fn enter_scope(&mut self) -> LocalScope {
@@ -101,14 +93,18 @@ impl<'a> Scope for LocalScope<'a> {
     }
 
     fn define(&mut self, name: &str, c: LocatedConstruct) -> pass::Result<()> {
-        if !self.scope_binds.insert(name.to_string()) {
+        if self
+            .stash
+            .insert(name.to_string(), self.map.remove(name))
+            .is_some()
+        {
             return Err(Error::duplicated_identifier(
                 name,
-                self.get(name).unwrap(),
+                self.stash.get(name).unwrap().unwrap(),
                 c,
             ));
         }
-        self.map.entry(name.to_string()).or_default().push(c);
+        self.map.insert(name.to_string(), c);
         Ok(())
     }
 }
